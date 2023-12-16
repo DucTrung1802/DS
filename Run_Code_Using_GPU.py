a62ca811b19b1724de850128c4205c4738143249
Duc Trung <trung.lyduc18@gmail.com>
2023-12-16 22:25:14 +0700

    ADD: Run_Code_Using_GPU

--- Run_Code_Using_GPU.py
+++ Run_Code_Using_GPU.py
@@ -3,18 +3,21 @@ import numpy as np
 # to measure exec time
 from timeit import default_timer as timer 
 
+BENCHMARK_PARAMETER = 200000000
+
 # normal function to run on cpu
 def func(a):							 
-	for i in range(100000000):
+	for i in range(BENCHMARK_PARAMETER):
 		a[i]+= 1	
 
 # function optimized to run on gpu 
 @jit(target_backend='cuda')						 
 def func2(a):
-	for i in range(100000000):
+	for i in range(BENCHMARK_PARAMETER):
 		a[i]+= 1
+		
 if __name__=="__main__":
-	n = 100000000						
+	n = BENCHMARK_PARAMETER						
 	a = np.ones(n, dtype = np.float64)
 	
 	start = timer()
