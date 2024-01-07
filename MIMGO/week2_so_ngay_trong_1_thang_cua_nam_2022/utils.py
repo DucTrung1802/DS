837715bc51b5feeaaadb9d5bd52fd05b6f8b8e95
Duc Trung <trung.lyduc18@gmail.com>
2024-01-07 18:10:07 +0700

    ADD: week2_so_ngay_trong_1_thang_cua_nam_2022

---
+++ MIMGO/week2_so_ngay_trong_1_thang_cua_nam_2022/utils.py
@@ -0,0 +1,16 @@
+def numberDaysOfMonth(n):
+    kaka_dictionary = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31,
+    }
+    return kaka_dictionary[n]
