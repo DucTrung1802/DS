e0fc6447b349d72fc908874054d879a5c5429ac5
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-01-16 12:56:18 +0700

    COMPLETE: week3_phan_so

--- MIMGO/week3_phan_so/Frac.py
+++ MIMGO/week3_phan_so/Frac.py
@@ -2,50 +2,53 @@ class Fraction:
     def __init__(self, numerator, denominator):
         if denominator == 0:
             raise Exception("No denominators equal to 0")
-        self.numerator = numerator
-        self.denominator = denominator
+        self.numerator = int(numerator)
+        self.denominator = int(denominator)
         self.compact()
 
     def show(self):
         print(self.numerator, "/", self.denominator)
 
     def compact(self):
-        if self.numerator > self.denominator:
-            for i in reversed(range(2, int(self.denominator) + 1)):
-                if (self.numerator % i == 0) and (self.denominator % i == 0):
-                    self.numerator /= i
-                    self.denominator /= i
+        for i in reversed(range(2, int(self.denominator) + 1)):
+            if self.numerator % i == 0 and self.denominator % i == 0:
+                self.numerator //= i
+                self.denominator //= i
 
     def add(self, fraction2):
-        self.numerator = (
+        self.numerator = int(
             self.numerator * fraction2.denominator
             + self.denominator * fraction2.numerator
         )
-        self.denominator *= fraction2.denominator
+        self.denominator *= int(fraction2.denominator)
         self.compact()
+        return self
 
     def sub(self, fraction2):
-        self.numerator = (
+        self.numerator = int(
             self.numerator * fraction2.denominator
             - self.denominator * fraction2.numerator
         )
-        self.denominator *= fraction2.denominator
+        self.denominator *= int(fraction2.denominator)
         self.compact()
+        return self
 
     def mul(self, fraction2):
-        self.numerator *= fraction2.numerator
-        self.denominator *= fraction2.denominator
+        self.numerator *= int(fraction2.numerator)
+        self.denominator *= int(fraction2.denominator)
         self.compact()
+        return self
 
     def truediv(self, fraction2):
         if fraction2.numerator == 0:
             raise Exception("Divide by Zero")
-        self.numerator *= fraction2.denominator
-        self.denominator *= fraction2.numerator
+        self.numerator *= int(fraction2.denominator)
+        self.denominator *= int(fraction2.numerator)
         self.compact()
+        return self
 
 
-# myfrac1 = Fraction(19, 12)
+# myfrac1 = Fraction(-2, 24)
 # myfrac2 = Fraction(3, 4)
 # myfrac1.add(myfrac2)
 # myfrac1.show()
