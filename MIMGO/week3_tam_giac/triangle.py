ad623b52489e02e2c14c82c8f3a2a4b4b398c24b
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-01-16 15:12:06 +0700

    COMPLETE: week3_tam_giac

--- MIMGO/week3_tam_giac/triangle.py
+++ MIMGO/week3_tam_giac/triangle.py
@@ -1,22 +1,21 @@
-from math import sqrt, cos
+from math import sqrt
 
 
 class Triangle:
     """Define a triangle."""
 
     def __init__(self, a, b, c):
-        """Defile a triangle with 3 edges."""
+        """Define a triangle with 3 edges."""
         self.a = a
         self.b = b
         self.c = c
-        return self.is_triangle()
 
     def calculate_perimeter(self):
         return self.a + self.b + self.c
 
     def calculate_area(self):
         s = self.calculate_perimeter() / 2
-        return sqrt(s(s - self.a)(s - self.b)(s - self.c))
+        return sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
 
     def is_triangle(self):
         if (
@@ -29,26 +28,65 @@ class Triangle:
             return False
 
     def check_equilateral_triangle(self):
-        return self.a == self.b == self.c
+        return self.a == self.b and self.a == self.c
 
     def check_isosceles_triangle(self):
         return (
-            (self.a == self.b != self.c)
-            or (self.a == self.b != self.c)
-            or (self.b == self.c != self.a)
+            (self.a == self.b and self.a != self.c)
+            or (self.a == self.c and self.a != self.b)
+            or (self.b == self.c and self.b != self.a)
         )
 
     def calculate_cos_of_biggest_angle(self):
-        
+        if (self.a >= self.b) and (self.a >= self.c):
+            return (pow(self.b, 2) + pow(self.c, 2) - pow(self.a, 2)) / (
+                2 * self.b * self.c
+            )
+        elif (self.b >= self.a) and (self.b >= self.c):
+            return (pow(self.a, 2) + pow(self.c, 2) - pow(self.b, 2)) / (
+                2 * self.a * self.c
+            )
+        elif (self.c >= self.a) and (self.c >= self.b):
+            return (pow(self.a, 2) + pow(self.b, 2) - pow(self.c, 2)) / (
+                2 * self.a * self.b
+            )
 
     def check_square_triangle(self):
-        pass
-
-    def check_normal_triangle(self):
-        pass
+        if (
+            (pow(self.a, 2) == pow(self.b, 2) + pow(self.c, 2))
+            or (pow(self.b, 2) == pow(self.a, 2) + pow(self.c, 2))
+            or (pow(self.c, 2) == pow(self.a, 2) + pow(self.b, 2))
+        ):
+            return True
+        return False
 
     def check_obtuse_triangle(self):
-        pass
+        if (
+            (pow(self.a, 2) > pow(self.b, 2) + pow(self.c, 2))
+            or (pow(self.b, 2) > pow(self.a, 2) + pow(self.c, 2))
+            or (pow(self.c, 2) > pow(self.a, 2) + pow(self.b, 2))
+        ):
+            return True
+        return False
 
     def check_triangle_type(self):
-        pass
+        if self.check_equilateral_triangle():
+            return "equilateral triangle"
+        elif self.check_isosceles_triangle():
+            return "isosceles triangle"
+        elif self.check_square_triangle():
+            return "square triangle"
+        elif self.check_obtuse_triangle():
+            return "obtuse triangle"
+        else:
+            return "normal triangle"
+
+    def check_normal_triangle(self):
+        if (
+            self.check_equilateral_triangle()
+            or self.check_isosceles_triangle()
+            or self.check_square_triangle()
+            or self.check_obtuse_triangle()
+        ):
+            return False
+        return True
