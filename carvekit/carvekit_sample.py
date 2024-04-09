65cc75c079d474f1bf5f6af48c0009e596718d2e
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-04-09 23:56:58 +0700

    UPDATE: carvekit_sample

--- carvekit/carvekit_sample.py
+++ carvekit/carvekit_sample.py
@@ -7,6 +7,7 @@ from carvekit.pipelines.postprocessing import MattingMethod
 from carvekit.pipelines.preprocessing import PreprocessingStub
 from carvekit.trimap.generator import TrimapGenerator
 
+BASE_PATH = "./images/"
 INPUT_IMAGE_PATH = "car_1.jpg"
 
 OUTPUT_IMAGE_PATH = INPUT_IMAGE_PATH.rsplit(".", 1)[0] + "_rmbg.png"
@@ -30,6 +31,6 @@ interface = Interface(
     pre_pipe=preprocessing, post_pipe=postprocessing, seg_pipe=seg_net
 )
 
-image = PIL.Image.open(INPUT_IMAGE_PATH)
+image = PIL.Image.open(BASE_PATH + INPUT_IMAGE_PATH)
 cat_wo_bg = interface([image])[0]
-cat_wo_bg.save(OUTPUT_IMAGE_PATH)
+cat_wo_bg.save(BASE_PATH + OUTPUT_IMAGE_PATH)
